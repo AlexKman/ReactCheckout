64ac4f41af671f0dd53a02f75da771a52844158e
Desktopy12 <Keeley.lp@gmail.com>
2020-06-07 15:48:38 +0100

    removed unwanted code, added itemss variable

--- react-checkout/src/App.js
+++ react-checkout/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const items = [
+    { item: "A", UnitPrice: 50 },
+    { item: "B", UnitPrice: 30 },
+    { item: "C", UnitPrice: 20 },
+    { item: "D", UnitPrice: 15 }
+  ];
+  return <div className="App"></div>;
 }
 
 export default App;
