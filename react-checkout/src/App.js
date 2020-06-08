cfd10a215f9d3e91ab1b3ee0885f9c1e2db77b3f
Desktopy12 <Keeley.lp@gmail.com>
2020-06-08 02:45:29 +0100

    Added basic header and footer which will eventually contain the calculated total

--- react-checkout/src/App.js
+++ react-checkout/src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "../src/footer.css";
+
+function Header(props) {
+  return <h1>Shopping Cart</h1>;
+}
+
+function Footer(props) {
+  return <h2>Footer</h2>;
+}
 
 function App() {
-  const items = [
+  const Items = [
     { item: "A", UnitPrice: 50 },
     { item: "B", UnitPrice: 30 },
     { item: "C", UnitPrice: 20 },
     { item: "D", UnitPrice: 15 }
   ];
-  return <div className="App"></div>;
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
